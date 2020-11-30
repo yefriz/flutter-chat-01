298aeb1c13c712d37b5be3afd840e8ea864bf1e9
genfiz <genfizsantiago@gmail.com>
2020-11-30 01:07:07 -0400

    frontend de login y registro terminado

---
+++ lib/routes/routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:chat/pages/chat_page.dart';
+import 'package:chat/pages/loading_page.dart';
+import 'package:chat/pages/login_page.dart';
+import 'package:chat/pages/register_page.dart';
+import 'package:chat/pages/usuario_page.dart';
+import 'package:flutter/cupertino.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+  'usuarios': (_) => UsuariosPage(),
+  'chat': (_) => ChatPage(),
+  'login': (_) => LoginPage(),
+  'register': (_) => RegisterPage(),
+  'loading': (_) => LoadingPage()
+};
