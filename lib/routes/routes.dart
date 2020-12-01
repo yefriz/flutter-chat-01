bc4ed7825cf491ba346a6fec3ca7c79c3ba95020
genfiz <genfizsantiago@gmail.com>
2020-12-01 00:40:58 -0400

    Fin seccion 7

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -3,7 +3,7 @@ import 'package:chat/pages/chat_page.dart';
 import 'package:chat/pages/loading_page.dart';
 import 'package:chat/pages/login_page.dart';
 import 'package:chat/pages/register_page.dart';
-import 'package:chat/pages/usuario_page.dart';
+import 'package:chat/pages/usuarios_page.dart';
 import 'package:flutter/cupertino.dart';
 
 final Map<String, Widget Function(BuildContext)> appRoutes = {
---
+++ lib/widgets/chat_message.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  final String texto;
+  final String uid;
+  final AnimationController animationController;
+  const ChatMessage(
+      {Key key,
+      @required this.texto,
+      @required this.uid,
+      @required this.animationController})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: animationController,
+      child: SizeTransition(
+        sizeFactor:
+            CurvedAnimation(parent: animationController, curve: Curves.easeOut),
+        child: Container(
+          child: this.uid == '123' ? _myMessage() : _notMessage(),
+        ),
+      ),
+    );
+  }
+
+  Widget _myMessage() {
+    return Align(
+      alignment: Alignment.centerRight,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(right: 5, bottom: 10, left: 30),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.white),
+        ),
+        decoration: BoxDecoration(
+            color: Color(0xff4D9EF6), borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+  }
+
+  Widget _notMessage() {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        margin: EdgeInsets.only(left: 5, bottom: 10, right: 30),
+        child: Text(
+          this.texto,
+          style: TextStyle(color: Colors.black),
+        ),
+        decoration: BoxDecoration(
+            color: Color(0xffE4E5E8), borderRadius: BorderRadius.circular(20)),
+      ),
+    );
+  }
+}
